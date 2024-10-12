25adf6644ae441cfedb709bda6a9dbbd3c629253
Noureldin-Ahmed-FullStack <noureldin2662002@gmail.com>
2024-10-13 01:17:30 +0300

    social page

--- src/components/Pages/SocialPage.tsx
+++ src/components/Pages/SocialPage.tsx
@@ -1,11 +1,27 @@
-import { useEffect } from "react"
-
+import { SocialPost } from "../../types";
+import { SinglePost } from "../ui/SinglePost";
+const postsArray: SocialPost[] = [{
+  title: "Hello World",
+  content: "loream awdwa dawdwad waodpwadka opwadkwaopdkwa waopdkwaopdk dwakopdwakd"
+}, {
+  title: "Hello World",
+  content: "loream awdwa dawdwad waodpwadka opwadkwaopdkwa waopdkwaopdk dwakopdwakd",
+  image:"https://ssniper.sirv.com/Images/other%20projects/compressed-image%20(1).jpg"
+}, {
+  title: "Hello World",
+  content: "loream awdwa dawdwad waodpwadka opwadkwaopdkwa waopdkwaopdk dwakopdwakd"
+},]
 export default function SocialPage() {
-    useEffect(() => {
-      
-    }, [])
-    
+
   return (
-    <div>SocialPage</div>
+    <div className="mt-28 flex justify-center">
+      <div className="flex flex-col">
+        {postsArray.map((item,index) => (
+          <div className="my-4" key={index}>
+            <SinglePost content={item.content} title={item.title} image={item.image}/>
+          </div>
+        ))}
+      </div>
+    </div>
   )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,24 +17,33 @@ module.exports = {
     extend: {
       animation: {
         aurora: "aurora 60s linear infinite",
+        "meteor-effect": "meteor 5s linear infinite",
       },
       keyframes: {
-        aurora: {
-          from: {
-            backgroundPosition: "50% 50%, 50% 50%",
+        meteor: {
+          "0%": { transform: "rotate(215deg) translateX(0)", opacity: "1" },
+          "70%": { opacity: "1" },
+          "100%": {
+            transform: "rotate(215deg) translateX(-500px)",
+            opacity: "0",
           },
-          to: {
-            backgroundPosition: "350% 50%, 350% 50%",
+        },
+          aurora: {
+            from: {
+              backgroundPosition: "50% 50%, 50% 50%",
+            },
+            to: {
+              backgroundPosition: "350% 50%, 350% 50%",
+            },
           },
         },
       },
     },
-  },
-  plugins: [addVariablesForColors],
-};
+    plugins: [addVariablesForColors],
+  };
 
-// This plugin adds each Tailwind color as a global CSS variable, e.g. var(--gray-200).
-function addVariablesForColors({ addBase, theme }: any) {
+  // This plugin adds each Tailwind color as a global CSS variable, e.g. var(--gray-200).
+  function addVariablesForColors({ addBase, theme }: any) {
   let allColors = flattenColorPalette(theme("colors"));
   let newVars = Object.fromEntries(
     Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
